d1af903d04026154778e9a0965b712c1393020d1
kundan200708 <kk1722035@gmail.com>
2026-08-27 23:54:34 +0530

    11 Function Complete

---
+++ 11 Function.c/00 Intro.c
@@ -0,0 +1,26 @@
+/*
+Function:- 
+         --> A function is the basic building block of a program that helps the programmers to writes the 
+         code and execute them seperately.
+         
+         --> Function leads code reusability that is you can write the code at once and execute whenever needed.
+
+         --> C - Language consists on several function called pre-defined function or in-built function.
+
+         --> Moreover programmers can also  define their own function for specific purpose called use define function.
+
+         --> An user define function is similar to pre-define function that helps programmers to decompose a large
+         problem into seperate sub-problem and code them individually and finally you can assemble all function within
+         a single module to accomplish the tasks.
+         
+
+
+
+
+
+
+
+
+
+
+*/
